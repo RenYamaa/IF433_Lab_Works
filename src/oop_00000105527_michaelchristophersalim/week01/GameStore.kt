59febc027f28ad56cd9be313569dff406948f77c
Michael <michaelgrowt24@gmail.com>
2026-02-07 14:25:02 +0700

    fix: adding gameTitle and price value

--- src/oop_00000105527_michaelchristophersalim/week01/GameStore.kt
+++ src/oop_00000105527_michaelchristophersalim/week01/GameStore.kt
@@ -1,8 +1,14 @@
 package oop_00000105527_michaelchristophersalim.week01
 
 fun main() {
-    val gameTitle: String
-    val price: Int
+    val gameTitle: String = "Simple Game Store"
+    val price: Int = 600000
+
+
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) price - (price * 0.2).toInt() else price - (price * 0.1).toInt()
+
+fun printReceipt(title: String, finalPrice: Int){
+    println("$title, $finalPrice")
+}
