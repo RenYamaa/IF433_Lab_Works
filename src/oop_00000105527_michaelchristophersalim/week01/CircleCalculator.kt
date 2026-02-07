791d9969df01587aa5d6e8c4a3100064f78662c6
Michael <michaelgrowt24@gmail.com>
2026-02-07 14:12:12 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000105527_michaelchristophersalim/week01/CircleCalculator.kt
+++ src/oop_00000105527_michaelchristophersalim/week01/CircleCalculator.kt
@@ -1,17 +1,13 @@
 package oop_00000105527_michaelchristophersalim.week01
 
-fun main(args: Array<String>) {
-    //Var Def
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     var area: Double = pi * radius * radius
 
-    // Output Concat
     println("Radius: $radius, Area: $area")
 
-    //Login check
     println(checkSize(area))
 }
 
