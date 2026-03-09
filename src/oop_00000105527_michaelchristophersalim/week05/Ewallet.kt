1189e2a441f0b43fbfbfbeb80580cb684ca1ef28
Michael <michaelgrowt24@gmail.com>
2026-03-09 17:26:55 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000105527_michaelchristophersalim/week05/Ewallet.kt
@@ -0,0 +1,18 @@
+package oop_00000105527_michaelchristophersalim.week05
+
+class Ewallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran sebear $amount berhasil dengan E-Wallet. Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup untuk pembayaran sebesar $amount")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance -= amount
+        println("Top up berhasil. Saldo sekarang: $balance")
+    }
+}
