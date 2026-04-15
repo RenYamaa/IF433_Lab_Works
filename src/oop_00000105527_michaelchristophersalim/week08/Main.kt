8f406e07421273e1a5a7836ca72d3cf86dacca22
RenYamaa <michaelgrowt24@gmail.com>
2026-04-15 11:01:50 +0700

    week08(trial): trigger NullPointerException with !!

--- src/oop_00000105527_michaelchristophersalim/week08/Main.kt
+++ src/oop_00000105527_michaelchristophersalim/week08/Main.kt
@@ -39,4 +39,12 @@ fun main() {
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast + fallback: $safeString")
     //Sudah di test
+
+    println("\n=== TEST THE RED BUTTON (!!) ===")
+    val toxicData: String? = null
+    try{
+        val length = toxicData!!.length
+    } catch (e: NullPointerException){
+        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
+    }
 }
