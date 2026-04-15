d1b0fd76512d45c38933039403bb5addf9b472d8
RenYamaa <michaelgrowt24@gmail.com>
2026-04-15 11:43:31 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000105527_michaelchristophersalim/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000105527_michaelchristophersalim.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        // Mengembalikan string transaksi mentah
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
