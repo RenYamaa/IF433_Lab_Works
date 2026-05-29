77a34dc29084cbb05a6951bd0441c1ab876863cc
RenYamaa <michaelgrowt24@gmail.com>
2026-05-29 12:15:14 +0700

    week13: create Student model

---
+++ src/oop_00000105527_michaelchristophersalim/week13/StudentSerialization.kt
@@ -0,0 +1,7 @@
+package oop_00000105527_michaelchristophersalim.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+// LANGKAH 1: Struktur Data Class
+data class Student(val name: String, val age: Int, val gpa: Double)
