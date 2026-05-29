b6e4c22d62f59a9b37f5d176341b23629cce51d5
RenYamaa <michaelgrowt24@gmail.com>
2026-05-29 12:29:25 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_00000105527_michaelchristophersalim/week13/CryptoTradeLogger.kt
+++ src/oop_00000105527_michaelchristophersalim/week13/CryptoTradeLogger.kt
@@ -52,4 +52,7 @@ fun main() {
     saveTrades(mockTrades, filePath)
 
     File(filePath).appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
+
+    val loadedData = loadTrades(filePath)
+    val totalPnl = loadedData.sumOf { it.pnl }
 }
